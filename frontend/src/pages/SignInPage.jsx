953671e5842f5eadeec214d54235d93c53b49f61
JoseMaria <josemarianunez451@gmail.com>
2026-02-09 22:42:24 -0300

    fix

--- frontend/src/pages/SignInPage.jsx
+++ frontend/src/pages/SignInPage.jsx
@@ -3,7 +3,8 @@ import { SignIn } from "@clerk/clerk-react";
 const SignInPage = () => {
   return (
     <div className="flex items-center justify-center py-12">
-      <SignIn routing="path" path="/sign-in" />
+      <SignIn routing="path" path="/sign-in" signUpUrl="/sign-up"
+       afterSignInUrl="/dashboard" />
     </div>
   );
 };
